4715031ba44acdb3eb2c1f463c0f8ffa3f3f55f7
mikeingley <michael.ingley@gmail.com>
2019-06-14 16:16:52 -0700

    1

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
-// write your code here
-
-});
+$(document).ready(function () {
+    $.getJSON('http://localhost:8080/data.json',
+        function (data) {
+            console.log(data)
+            var tr;
+            for (var i = 0; i < data.length; i++) {
+                tr = $('<tr>');
+                tr.append("<tr>");
+                tr.append("<td>" + data[i].name + "</td>");
+                tr.append("<td>" + data[i].description + "</td>");
+                tr.append("<td>" + 'https://www.google.com/maps?q=' + data[i].location + "</td>");
+                tr.append("</tr>");
+                $('table').append(tr);
+            }})});
